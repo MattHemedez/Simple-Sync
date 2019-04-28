a81c038aae6ff0541796996fa42fa5dbc04a0f06
MattHemedez <mhemedez@uci.edu>
2019-04-27 22:28:57 -0700

    Create function to list all files in Google Drive

--- synchronizer.py
+++ synchronizer.py
@@ -1,3 +1,13 @@
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+
+
 class Synchronizer:
     def __init__(self):
         pass
@@ -34,5 +44,45 @@ class Synchronizer:
         pass
 
 
+class GoogleDriveApiHandler:
+    def __init__(self):
+        self.creds = None
+        self.get_credentials()
+        self.service = build('drive', 'v3', credentials=self.creds)
+
+    def get_credentials(self):
+        self.creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle') and os.path.getsize('token.pickle') > 0:
+            with open('token.pickle', 'rb') as token:
+                self.creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not self.creds or not self.creds.valid:
+            if self.creds and self.creds.expired and self.creds.refresh_token:
+                self.creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                self.creds = flow.run_local_server()
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(self.creds, token)
+
+    def get_info_on_files(self):
+        temp = self.service.files().list(corpora="user", orderBy="name",
+                                         q="mimeType != 'application/vnd.google-apps.folder'",
+                                         fields="files(name, md5Checksum)").execute()
+        result = {}
+        for file_info in temp["files"]:
+            result[file_info["name"]] = file_info.get("md5Checksum", None)
+
+        return result
+
+    def download_files
+
+
 if __name__ == "__main__":
-    print("There is no test in this file.")
+    myGoogleApiHandler = GoogleDriveApiHandler()
+    myGoogleApiHandler.get_files()
