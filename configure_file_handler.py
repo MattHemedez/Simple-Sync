6446685ac5828189ac52d83713811bf0235f2f2a
MattHemedez <mhemedez@uci.edu>
2019-05-29 20:38:50 -0700

    Implemented json read and write capabilities

--- configure_file_handler.py
+++ configure_file_handler.py
@@ -1,28 +1,46 @@
 from pathlib import Path
+import json
+
+
+CONF_FILE_PATH = Path("conf.json")
 
 
 class ConfigurationHandler:
     def __init__(self):
-        pass
+        self.conf = dict()
+        try:
+            if self.conf_file_exists():
+                self.read_conf()
+            else:
+                self.init_conf()
+        except json.decoder.JSONDecodeError:
+            self.init_conf()
 
-    def initialize_configuration(self):
-        pass
+    def init_conf(self):
+        self.conf = {"file_dir_path": str(Path("files/"))}
+        self.save_conf()
 
-    def conf_exists(self):
-        pass
+    def conf_file_exists(self):
+        return CONF_FILE_PATH.exists()
 
-    def change_file_folder_location(self, folder_path: Path):
-        pass
+    def change_conf_entry(self, conf_key: str, conf_value):
+        if conf_key in self.conf:
+            self.conf[conf_key] = conf_value
+        else:
+            raise KeyError(conf_key)
 
-    def get_file_folder_location(self):
-        pass
+    def get_conf_entry(self, key):
+        return self.conf[key]
 
     def save_conf(self):
-        pass
+        with open(CONF_FILE_PATH, "w") as conf_file:
+            json.dump(self.conf, conf_file)
 
     def read_conf(self):
-        pass
+        with open(CONF_FILE_PATH, "r") as conf_file:
+            self.conf = json.load(conf_file)
 
 
 if __name__ == "__main__":
-    pass
+    conf_handler = ConfigurationHandler()
+    print("File Directory Path: ", Path(conf_handler.get_conf_entry("file_dir_path")).resolve())
