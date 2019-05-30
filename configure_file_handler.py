2250771993bc9b311d18fa51109308ad7e527bcd
MattHemedez <mhemedez@uci.edu>
2019-05-29 19:59:25 -0700

    Added skeleton configuration handler class

---
+++ configure_file_handler.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+
+class ConfigurationHandler:
+    def __init__(self):
+        pass
+
+    def initialize_configuration(self):
+        pass
+
+    def conf_exists(self):
+        pass
+
+    def change_file_folder_location(self, folder_path: Path):
+        pass
+
+    def get_file_folder_location(self):
+        pass
+
+    def save_conf(self):
+        pass
+
+    def read_conf(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
